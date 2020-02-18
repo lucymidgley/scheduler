a45df3f50f1527c66789fd7f77c7ba643000fb7a
Lucy <lucymidgley13@gmail.com>
2020-02-18 16:15:17 -0700

    removed unused appointment data as now we fetch the appointments directly from the api

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,65 +7,6 @@ import Appointment from "components/Appointment";
 import getAppointmentsForDay from "../helpers/selectors.js"
 
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "3pm",
-//     interview: {
-//       student: "William Blake",
-//       interviewer: { 
-//         id: 2,
-//         name: "Tori Malcolm", 
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "5pm",
-//     interview: {
-//       student: "Emily Dickinson",
-//       interviewer: { 
-//         id: 3, 
-//         name: "Mildred Nazir", 
-//         avatar: "https://i.imgur.com/T2WwVfS.png" }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   },
-//   {
-//     id: 6,
-//     time: "5pm",
-//     interview: {
-//       student: "Robert Frost",
-//       interviewer: { 
-//         id: 4, 
-//         name: "Cohana Roy", 
-//         avatar: "https://i.imgur.com/FK8V841.jpg" 
-//       }
-//     }
-//   }
-// ];
-
-
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
