826899db85cd6c437c09300a50456c4c82d00cce
Lucy <lucymidgley13@gmail.com>
2020-02-17 19:17:21 -0700

    put the appointment together with the show and empty components and rendered them conditionallu and wrote stores to test this

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      { props.interview  ? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={props.onEdit} onDelete={props.onDelete}/> : <Empty onAdd={props.onAdd} /> }
+    </article>
   )
 };
+
+// import Confirm from "components/Appointment/Confirm.js";
+// import Status from "components/Appointment/Status.js";
+// import Error from "components/Appointment/Error.js";
+// import Form from "components/Appointment/Form.js";
+
+
--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { Fragment } from 'react'
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -140,8 +139,22 @@ storiesOf("Button", module)
         .addParameters({
             backgrounds: [{ name: "white", value: "#fff", default: true }]
           })
-        .add("Appointment", () => <Appointment />)
-        .add("Appointment with Time", () => <Appointment time="12pm" />)
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="12pm" />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
+        .add("Appointment Booked", () => (
+          <Fragment>
+            <Appointment
+              id={1}
+              time="12pm"
+              interview={{ student: "Lydia Miller-Jones", interviewer }}
+            />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
         .add("Show", () => (
@@ -175,5 +188,6 @@ storiesOf("Button", module)
        interviewers={interviewers}
        onSave={action("onSave")}
        onCancel={action("onCancel")} />
-       ));
+       ))
+      
 
