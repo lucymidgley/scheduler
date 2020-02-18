d90f88cd881d89a73351253b3ef6ad15b2fcd9e2
Lucy <lucymidgley13@gmail.com>
2020-02-17 18:16:44 -0700

    added the button component and assocaited stylkes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,19 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+
 export default function Button(props) {
-   return <></>;
-}
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+
+   return <button onClick={props.onClick} disabled={props.disabled} className={buttonClass}>{props.children}</button>;
+ }
+
+
+
+ 
+ 
+
