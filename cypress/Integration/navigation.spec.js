599929e4b2a7756c643127b334e2e294e506a235
Lucy <lucymidgley13@gmail.com>
2020-02-25 12:44:30 -0700

    added the tests to navigate to Tuesday and check that the day is selected properly

--- cypress/Integration/navigation.spec.js
+++ cypress/Integration/navigation.spec.js
@@ -4,30 +4,13 @@ describe("Navigation", () => {
     cy.contains("Monday")
   })
   it("should visit root", () => {
-    
   });
 
   it("should navigate to Tuesday", () => {
-    cy.get('li').contains("Tuesday")
-    .first()
-    .click();
+    
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .get('.day-list__item--selected').should('exist')
   });
 
-  // it("user can create an appointment", () => {
-  //   cy.get('[alt="Add"]')
-  //   .first()
-  //   .click()
-  //   cy.get('[data-testid=student-name-input]')
-  //   .type("Lydia Miller-Jones")
-
-  //   cy.get('.interviewers__item--selected').should('not.exist')
-
-  //   cy.get('.interviewers__item-image')
-  //   .first()
-  //   .click()
-
-  //   cy.get('.interviewers__item--selected').should('exist')
-
-  // });
 });
-
